225bc8b034754490611f875f73b612a51f4e12a2
Roman Strypa <gfdg@bbbb.yyy>
2022-01-09 17:30:16 +0200

    Add: new file

---
+++ client/src/app/shared/layouts/services/positions.service.ts
@@ -0,0 +1,18 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {Position} from '../interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PositionsService {
+  constructor(private http: HttpClient) {
+  }
+
+  fetch(categoryId: string): Observable<Position[]> {
+    return this.http.get<Position[]>(`/api/position/${categoryId}`)
+  } 
+
+}
