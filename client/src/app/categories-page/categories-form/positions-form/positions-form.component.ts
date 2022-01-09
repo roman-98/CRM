a02dcecfceb19ab8105e9a45a6a45f2a6c94ed4f
Roman Strypa <gfdg@bbbb.yyy>
2022-01-09 14:32:02 +0200

    Add: patamether 'categoryId'

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-positions-form',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./positions-form.component.css']
 })
 export class PositionsFormComponent implements OnInit {
+  @Input('categoryId') categoryId: string
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
