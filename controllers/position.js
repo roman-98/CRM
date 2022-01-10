446d9a03daf8253f216378f86ae6173e1a7feb24
Roman Strypa <gfdg@bbbb.yyy>
2022-01-10 22:36:10 +0200

    Fix: bug (line 21)

--- controllers/position.js
+++ controllers/position.js
@@ -18,7 +18,7 @@ module.exports.create = async function(req, res) {
   try {
     const position = await new Position({
       name: req.body.name,
-      cost: req.bady.cost,
+      cost: req.body.cost,
       category: req.body.category,
       user: req.user.id
     }).save()
