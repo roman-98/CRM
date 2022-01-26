fe96138cb0c18b4eba8b9e69ba9b369edb9e4b86
Roman Strypa <gfdg@bbbb.yyy>
2022-01-26 15:09:31 +0200

    Fix: method add (orderPosition)

--- client/src/app/order-page/order.service.ts
+++ client/src/app/order-page/order.service.ts
@@ -1,10 +1,20 @@
 import {Injectable} from "@angular/core";
+import {OrderPosition, Position} from "../shared/layouts/interfaces";
 
 @Injectable()
 export class OrderService {
+  
+  public list: OrderPosition[] = []
+  public price = 0
 
-  add(position: any) {
-
+  add(position: Position) {
+    const orderPosition: OrderPosition = Object.assign({}, {
+      name: position.name,
+      cost: position.cost,
+      quantity: position.quantity,
+      _id: position._id
+    })
+    this.list.push(orderPosition)
   }
 
   remove() {
