eb59b985f51b4da1fdea0fd4fec05100585e327f
Roman Strypa <gfdg@bbbb.yyy>
2022-01-26 16:57:38 +0200

    Fix: order (number, price, remove)

--- client/src/app/order-page/order.service.ts
+++ client/src/app/order-page/order.service.ts
@@ -14,15 +14,33 @@ export class OrderService {
       quantity: position.quantity,
       _id: position._id
     })
-    this.list.push(orderPosition)
+
+    const candidate = this.list.find(p => p._id === orderPosition._id)
+
+    if (candidate) {
+      // Change number
+      candidate.quantity += orderPosition.quantity
+    } else {
+      this.list.push(orderPosition)
+    }
+
+    this.computePrice()
   }
 
-  remove() {
-
+  remove(orderPosition: OrderPosition) {
+    const index = this.list.findIndex(p => p._id === orderPosition._id)
+    this.list.splice(index, 1)
+    this.computePrice()
   }
   
-  close() {
+  clear() {
     
+  }
+
+  private computePrice() {
+    this.price = this.list.reduce((total, item) => {
+      return total += item.quantity * item.cost
+    }, 0)
   }
 
 }
