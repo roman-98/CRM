d35fd7b12ed70a75b8bcb408b8fe5548c5b28496
Roman Strypa <gfdg@bbbb.yyy>
2022-01-26 15:05:03 +0200

    Add: two interfaces (Order, OrderPosition)

--- client/src/app/shared/layouts/interfaces.ts
+++ client/src/app/shared/layouts/interfaces.ts
@@ -22,3 +22,18 @@ export interface Position {
    _id?: string
    quantity?: number
 }
+
+export interface Order {
+  date?: Date
+  order?: number
+  user?: string
+  list: OrderPosition[]
+  _id?: string
+}
+
+export interface OrderPosition {
+  name: string
+  cost: number
+  quantity: number
+  _id?: string
+}
